13a7e374c821adc572445eba59a0d8cf8e5983bd
seventhsnow <seventhsnow@seventhsnows-iMac.local>
2019-12-21 21:26:01 -0800

    initializing ios app

---
+++ first/first/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  first
+//
+//  Created by seventhsnow on 12/21/19.
+//  Copyright © 2019 seventhsnow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
